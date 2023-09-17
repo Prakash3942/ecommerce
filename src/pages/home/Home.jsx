189008a157004a70127933182a03aaa7a2b75ddd
Prakash3942 <prakashpatasani60@gmail.com>
2023-09-17 13:01:16 +0530

    filter and product cart design complete

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,11 +2,19 @@ import React, { useContext } from "react";
 import Layout from "../../components/layout/Layout";
 
 import HeroSection from "../../components/heroSection/HeroSection";
+import Filter from "../../components/filter/Filter";
+import ProductCard from "../../components/productCard/ProductCard";
+import Testimonial from "../../components/testimonial/Testimonial";
+import Track from "../../components/track/Track";
 
 function Home() {
   return (
     <Layout>
       <HeroSection />
+      <Filter />
+      <ProductCard />
+      <Track />
+      <Testimonial />
     </Layout>
   );
 }
